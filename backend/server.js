f8fb5db965e5b680d8a62a2487fd0b3e9406d284
nastyaklypak <anastasiaklypak@gmail.com>
2025-11-30 18:38:36 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./services/db.js";
-import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import newsRoutes from "./routes/newsRoutes.js";
 
 dotenv.config();
+
 const app = express();
 
 // CORS
@@ -12,21 +14,31 @@ const allowedOrigins = process.env.CLIENT_ORIGINS.split(",");
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
     callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
+// Middleware
 app.use(express.json());
 
 // Routes
-app.use("/products", productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/news", newsRoutes);
 
-// Connect to DB
-connectDB(process.env.MONGO_URI);
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "Backend працює! " });
+});
+
+// DB Connection
+const dbURI = process.env.MONGO_URI;
+connectDB(dbURI);
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
